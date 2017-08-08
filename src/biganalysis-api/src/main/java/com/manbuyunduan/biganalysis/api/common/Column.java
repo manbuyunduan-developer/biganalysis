e7044e5ca0f2d5fbea00c8b7089d420ae68714e5
manbuyunduan-developer <384057431@qq.com>
2017-08-08 23:20:08 +0800

    新增Column文件

---
+++ src/biganalysis-api/src/main/java/com/manbuyunduan/biganalysis/api/common/Column.java
@@ -0,0 +1,19 @@
+package com.manbuyunduan.biganalysis.api.common;
+
+/**
+ * Created by Administrator on 2017/8/8.
+ */
+public class Column {
+
+    /**
+     * 
+     * @param a
+     * @return
+     */
+
+    public String a(String a)
+    {
+        System.out.print(a);
+        return a;
+    }
+}
